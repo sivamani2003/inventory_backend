06145048d4689fb2251e53331e25d925f70acf8b
siva <sivamani_k@srmap.edu.in>
2024-05-29 14:14:01 +0530

    update

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+dotenv.config()
+const app = express()
+const port = process.env.PORT 
+const corsOptions = {
+    origin:true,
+    credentials:true
+}
+mongoose.set('strictQuery',false)
+const connect= async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
+        console.log('Mongodb database connected')
+    } catch (err) {
+        console.log('Mongodb database connection failed')
+    }
+}
+app.use(express.json())
+app.use(cors(corsOptions))
+app.listen(port,()=>{
+    connect();
+    console.log('server listening on port',port)
+})
